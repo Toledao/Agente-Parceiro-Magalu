15c2e78038ea3b2353d8f3d00d6e7d8267369218
Matheus Pereira <matheuspereira@Matheuss-Mac-Pro.local>
2022-04-16 15:07:23 -0300

    Instalação Package EStylesheet

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './sources/pages/Login';
 
 export default function AgenteParceiro() {
+
+  const Stack = createNativeStackNavigator();
+
  return (
-   <SafeAreaView>
-     <Text>
-       ALOU MAGALOUU
-     </Text>
-   </SafeAreaView>
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName='Login'>
+      <Stack.Screen name="Login" component={Login}/>
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
