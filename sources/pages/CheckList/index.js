a2a80a9fbff2356838fdd457580d9287ade4134f
Joao-Tome <73352040+Joao-Tome@users.noreply.github.com>
2022-04-24 21:48:08 -0300

    correção de mini erro

--- sources/pages/CheckList/index.js
+++ sources/pages/CheckList/index.js
@@ -1,6 +1,6 @@
 import React, {Fragment, useState} from 'react';
 import { View, SafeAreaView, Text, ScrollView, TextInput, FlatList, TouchableOpacity } from 'react-native';
-import Styles from './styles';
+import Styles from './Styles';
 import SelectMultiple from 'react-native-select-multiple'
 
 export default function CheckList() {
