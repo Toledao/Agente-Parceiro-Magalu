bf2bea0a6b4063c864a0d907cc3e2c22e27c605d
Joao-Tome <73352040+Joao-Tome@users.noreply.github.com>
2022-04-26 18:55:43 -0300

    Atalhos Reworkado Arrumar o Atalhos Icon #32

--- sources/components/AtalhosIcon/index.js
+++ sources/components/AtalhosIcon/index.js
@@ -1,63 +1,22 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, ScrollView} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import Icon from 'react-native-vector-icons/Ionicons'
-import { useNavigation } from '@react-navigation/native';
+import Atalho from '../Atalho';
 
 export default function AtalhosIcon() {
-    const navigation = useNavigation()
+
  return (
     <ScrollView style={Styles.areaAtalho} horizontal={true} showsHorizontalScrollIndicator={false} >
-
-        {/* Atalho 1 */}
-
-        <TouchableOpacity style={Styles.atalho} onPress={() => navigation.navigate('Detalhes')}>  
-            {/* Botei para voltar ao Login, dps trocar para a tela especifico */}
-            <View style={Styles.areaIMG} >
-                <Icon name="list" size={40}/>
-            </View>
-            <Text style={Styles.txtAtalho}> Checklist Avulso </Text>
-        </TouchableOpacity>
-
-        {/* Atalho 2 */}
-
-        <TouchableOpacity style={Styles.atalho} onPress={() => navigation.navigate('ListaRoteiro')}>  
-            {/* Botei para voltar ao Login, dps trocar para a tela especifico */}
-            <View style={Styles.areaIMG} >
-                <Icon name="beer-outline" size={40}/>
-            </View>
-            <Text style={Styles.txtAtalho}> Checklist Avulso 2 </Text>
-        </TouchableOpacity>
-
-        {/* Atalho 3 */}
-
-        <TouchableOpacity style={Styles.atalho} onPress={() => navigation.navigate('Login')}>  
-            {/* Botei para voltar ao Login, dps trocar para a tela especifico */}
-            <View style={Styles.areaIMG} >
-                <Icon name="beer-outline" size={40}/>
-            </View>
-            <Text style={Styles.txtAtalho}> Checklist Avulso 3 </Text>
-        </TouchableOpacity>
-
-        {/* Atalho 4 */}
-
-        <TouchableOpacity style={Styles.atalho} onPress={() => navigation.navigate('Login')}>  
-            {/* Botei para voltar ao Login, dps trocar para a tela especifico */}
-            <View style={Styles.areaIMG} >
-                <Icon name="beer-outline" size={40}/>
-            </View>
-            <Text style={Styles.txtAtalho}> Checklist Avulso 4</Text>
-        </TouchableOpacity>
-
-        {/* Atalho 5 */}
-
-        <TouchableOpacity style={Styles.atalho} onPress={() => navigation.navigate('Login')}>  
-            {/* Botei para voltar ao Login, dps trocar para a tela especifico */}
-            <View style={Styles.areaIMG} >
-                <Icon name="beer-outline" size={40}/>
-            </View>
-            <Text style={Styles.txtAtalho}> Checklist Avulso 5</Text>
-        </TouchableOpacity>
+        <Atalho icone='list' navigate='Descricao' nomeatalho="Checklist Avulso" />
+        <Atalho icone='list' navigate='Descricao' nomeatalho="Checklist Avulso" />
+        <Atalho icone='list' navigate='Descricao' nomeatalho="Checklist Avulso" />
+        <Atalho icone='list' navigate='Descricao' nomeatalho="Checklist Avulso" />
+        <Atalho icone='list' navigate='Descricao' nomeatalho="Checklist Avulso" />
+        <Atalho icone='list' navigate='Descricao' nomeatalho="Checklist Avulso" />
+        <Atalho icone='list' navigate='Descricao' nomeatalho="Checklist Avulso" />
+        <Atalho icone='list' navigate='Descricao' nomeatalho="Checklist Avulso" />
+        
+        
     </ScrollView>
     
   );
